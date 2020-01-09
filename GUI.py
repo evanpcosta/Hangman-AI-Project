b43ae9680029390e56334a68c1b11634e0c9d48e
evancosta <evanpcosta@gmail.com>
2020-01-09 14:51:03 -0600

    add past comments for future reference

--- GUI.py
+++ GUI.py
@@ -1,19 +1,22 @@
 from tkinter import *
 
 
-#raises frame; puts first frame entry into second frame label
+# raises frame; puts first frame entry into second frame label
 def raise_frame_for(frame):
     global labellist
-    labellist =[]
+    labellist = []
     global result
     result = entry1.get()
-    print(result)
-    create_under_labels(result)
-    entry1.delete(0, END)
-    if len(result) > 0:
+    if 40 > len(result) > 0:
+        print(len(result))
+        create_under_labels(result)
         frame.tkraise()
+        print(result)
+        entry1.delete(0, END)
+    else:
+        label_too_large.config(text="Entry too large (40 character max)")
+        print(len(result))
     return
-
 
 
 def create_under_labels(word):
@@ -25,7 +28,7 @@ def create_under_labels(word):
     under.set(' __')
     y = 0
     for x in range(len(word)):
-        if x < 30:
+        if x < 20:
             if word[x] == ' ':
                 labellist.append(Label(second_frame, textvariable=blank).place(relx=find_relx(x), rely=0.56, anchor=CENTER))
             else:
@@ -36,52 +39,52 @@ def create_under_labels(word):
             else:
                 labellist.append(Label(second_frame, textvariable=under).place(relx=find_relx(y), rely=0.6, anchor=CENTER))
                 y = y + 1
-
+#problem with higher count letter words has to do with y and find relx probably with the result//2 calc
 
 def find_relx(x):
     print(result)
-    print(len(result)//2)
+    print(len(result) // 2)
     print(x)
-    if x == (len(result)//2):
+    if x == (len(result) // 2):
         rx = 0.5
         print(rx)
-        return(rx)
-    elif x < (len(result)//2):
-        nx = 0.5 - (((len(result)//2) - x)*0.04)
+        return (rx)
+    elif x < (len(result) // 2):
+        nx = 0.5 - (((len(result) // 2) - x) * 0.04)
         print(nx)
         return nx
-    elif x > (len(result)//2):
-        px = 0.5 + ((x - (len(result) // 2))*0.04)
+    elif x > (len(result) // 2):
+        px = 0.5 + ((x - (len(result) // 2)) * 0.04)
         print(px)
         return px
 
 
-
 def create_letter_labels(word):
-    labelletterlist = []
     for x in range(len(word)):
         if x > 30:
-            labellist.append(Label(second_frame, text="").place(relx=0.1*x, rely=0.6, anchor=CENTER))
+            labellist.append(Label(second_frame, text="").place(relx=0.1 * x, rely=0.6, anchor=CENTER))
         else:
             labellist.append(Label(second_frame, text="").place(relx=0.1, rely=0.62, anchor=CENTER))
     return labellist
 
 
-#changes the frame
+# changes the frame
 def raise_frame_bac(frame):
     entry2.delete(0, END)
     frame.tkraise()
     clear = ' '
     under.set('')
     blank.set('')
-    #spacelist[:] = []
+    label_too_large.config(text=" ")
+    # spacelist[:] = []
     guesslabel.config(text=" ")
     guesslist[:] = []
 
 
-
-#if guessed correctly turns an underline into a letter
+# if guessed correctly turns an underline into a letter
 guesslist = []
+
+
 def change_underline():
     letter = entry2.get()
     lisr = list(result)
@@ -91,12 +94,9 @@ def change_underline():
         for x in range(len(result)):
             if letter == lisr[x]:
                 break
-            #else:
-                #turn show in the guessed wrong section
+            # else:
+            # turn show in the guessed wrong section
         guesslabel.config(text="")
-
-
-
 
 
 root = Tk()
@@ -112,8 +112,11 @@ first_frame.place(x=0, y=0, width=500, height=500)
 label_question = Label(first_frame, text="What word would you like your opponent to guess?")
 label_question.place(relx=0.5, rely=0.3, anchor=CENTER)
 
+label_too_large = Label(first_frame, text="")
+label_too_large.place(relx=0.5, rely=0.45, anchor=CENTER)
+
 entry1 = Entry(first_frame, width=20)
-#entry1.focus()
+# entry1.focus()
 entry1.place(relx=0.5, rely=0.4, anchor=CENTER)
 
 button_1 = Button(first_frame, text="Next", command=lambda: raise_frame_for(second_frame)).place(x=220, y=250)
@@ -122,7 +125,7 @@ label_guess = Label(second_frame, text="Guess a letter:")
 label_guess.place(relx=0.2, rely=0.7, anchor=CENTER)
 
 entry2 = Entry(second_frame)
-#entry2.focus()
+# entry2.focus()
 entry2.place(relx=0.5, rely=0.7, anchor=CENTER)
 
 button_2 = Button(second_frame, text="Back", command=lambda: raise_frame_bac(first_frame)).place(x=10, y=10)
